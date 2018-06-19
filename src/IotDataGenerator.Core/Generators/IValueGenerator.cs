c4230616a1fb3d5b1a8f3aaa6dd86bf909c01fc6
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-06-19 17:07:39 +0200

    Add IValueGenerator

---
+++ src/IotDataGenerator.Core/Generators/IValueGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace IotDataGenerator.Core.Generators
+{
+    interface IValueGenerator<out T>
+    {
+        void Initialize();
+        T NextValue();
+    }
+}
