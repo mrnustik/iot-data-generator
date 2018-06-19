db958ea5774ad9f43206b27b87ebd9e4e3153daf
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-06-19 17:14:51 +0200

    Add DoubleVariable

---
+++ src/IotDataGenerator.Core/Variable/DoubleVariable.cs
@@ -0,0 +1,16 @@
+﻿using IotDataGenerator.Core.Generators;
+
+namespace IotDataGenerator.Core.Variable
+{
+    public class DoubleVariable : IVariable<double>
+    {
+        public string Name { get; }
+        public IValueGenerator<double> Generator { get; }
+
+        public DoubleVariable(string name, IValueGenerator<double> generator)
+        {
+            Name = name;
+            Generator = generator;
+        }
+    }
+}
