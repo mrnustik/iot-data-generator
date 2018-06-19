d91144b7cec788b0ee4f049ef84eecd8ae51643c
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-06-19 18:03:46 +0200

    Add DeviceManager class to manage device creation

---
+++ src/IotDataGenerator.Core/Devices/DeviceManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+namespace IotDataGenerator.Core.Devices
+{
+    public class DeviceManager : IDeviceManager
+    {
+        private readonly RegistryManager registryManager;
+        public DeviceManager(RegistryManager registryManager)
+        {
+            this.registryManager = registryManager;
+        }
+        
+        public Task<Device> CreateDevice(string name)
+        {
+            return registryManager.AddDeviceAsync(new Device(name));
+        }
+
+        public Task<Device> GetDevice(string name)
+        {
+            return registryManager.GetDeviceAsync(name);
+        }
+
+        public Task<Device> GetOrCreateDevice(string name)
+        {
+            try
+            {
+                return CreateDevice(name);
+            }
+            catch (Exception)
+            {
+                return GetDevice(name);
+            }
+        }
+    }
+}
