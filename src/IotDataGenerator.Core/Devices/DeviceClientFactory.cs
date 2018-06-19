4b6a08396629348d1f461fa430b7778321942b16
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-06-19 18:03:09 +0200

    Add DeviceClientFactory to create devices client to connect to IoT Hub

---
+++ src/IotDataGenerator.Core/Devices/DeviceClientFactory.cs
@@ -0,0 +1,24 @@
+﻿using IotDataGenerator.Core.Configuration;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.Configuration;
+using TransportType = Microsoft.Azure.Devices.Client.TransportType;
+
+namespace IotDataGenerator.Core.Devices
+{
+    class DeviceClientFactory : IDeviceClientFactory
+    {
+
+        private readonly IConfiguration configuration;
+
+        public DeviceClientFactory(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public DeviceClient CreateDeviceClient(Device device, TransportType transportType)
+        {
+            return DeviceClient.Create(configuration[ConfigurationKeys.IotHubUri], new DeviceAuthenticationWithRegistrySymmetricKey(device.Id, device.Authentication.SymmetricKey.PrimaryKey), transportType);
+        }
+    }
+}
