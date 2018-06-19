08e9a74f668170ba396d83fdc1a150592dbcc772
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-06-19 17:22:54 +0200

    Add IDeviceManager

---
+++ src/IotDataGenerator.Core/Devices/IDeviceManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Devices;
+
+namespace IotDataGenerator.Core.Devices
+{
+    interface IDeviceManager
+    {
+        Device CreateDevice(string name);
+        Device GetDevice(string name);
+        Device GetOrCreateDevice(string name);
+    }
+}
