cbc840a8389e020b9464b23fb336439514a184cf
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-06-19 17:07:55 +0200

    Add DoubleGenerator implementation of IValueGenerator

---
+++ src/IotDataGenerator.Core/Generators/DoubleGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IotDataGenerator.Core.Generators
+{
+    public class DoubleGenerator : IValueGenerator<double>
+    {
+        private readonly Random random = new Random();
+
+        public double MinimumValue { get; }
+        public double MaximumValue { get; }
+
+        public DoubleGenerator(double minimumValue, double maximumValue)
+        {
+            MinimumValue = minimumValue;
+            MaximumValue = maximumValue;
+        }
+
+        public void Initialize()
+        {
+        }
+
+        public double NextValue()
+        {
+            return random.NextDouble() * (MaximumValue - MinimumValue) + MinimumValue;
+        }
+    }
+}
