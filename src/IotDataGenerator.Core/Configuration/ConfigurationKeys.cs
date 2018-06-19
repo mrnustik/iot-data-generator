1b374825442bbecaec1083edd1d2a934c247b601
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-06-19 17:51:47 +0200

    Add ConfigurationKeys class to hold constant key values

---
+++ src/IotDataGenerator.Core/Configuration/ConfigurationKeys.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IotDataGenerator.Core.Configuration
+{
+    class ConfigurationKeys
+    {
+        public const string IotHubUri = "iotHubUri";
+    }
+}
