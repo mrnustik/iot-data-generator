d90c7372d613c89695e20ad5c88b0dc497f53faf
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-06-19 17:14:42 +0200

    Add IVariable interface

---
+++ src/IotDataGenerator.Core/Variable/IVariable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IotDataGenerator.Core.Generators;
+
+namespace IotDataGenerator.Core.Variable
+{
+    public interface IVariable<out T>
+    {
+        string Name { get; }
+        IValueGenerator<T> Generator { get; }
+    }
+}
